fd833e6e3404d892149f7640b044fd862e5d5f8f
hryesf <houriye.esfahanian@gmail.com>
2024-06-27 14:54:03 +0330

    refactor: set hasDuplicateCharacter method private

--- src/main/java/com/hryesf/Plugboard.java
+++ src/main/java/com/hryesf/Plugboard.java
@@ -48,7 +48,7 @@ public class Plugboard {
     }
 
     // this function will find the first duplicate character in input string
-    public boolean hasDuplicateCharacter(String input) {
+    private boolean hasDuplicateCharacter(String input) {
         final Set<Character> seen = new HashSet<>();
         for (char character : wires.toCharArray()) {
             if (seen.contains(character)){
