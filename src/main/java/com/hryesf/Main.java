cb33d1f94501872c2d311381af1a4e424760304c
hryesf <houriye.esfahanian@gmail.com>
2024-06-27 14:56:51 +0330

    feat: run the code!

--- src/main/java/com/hryesf/Main.java
+++ src/main/java/com/hryesf/Main.java
@@ -1,7 +1,11 @@
 package com.hryesf;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IllegalAccessException {
+        Plugboard plugboard = new Plugboard("ABCDEFGHIJKLMNOPQRST");
+        System.out.println(plugboard.process('A'));
+        System.out.println(plugboard.process('B'));
+        System.out.println(plugboard.process('X'));
+        System.out.println(plugboard.process('.'));
     }
 }
